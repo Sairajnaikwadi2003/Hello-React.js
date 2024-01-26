c83c5ce926b8d3aa77601bc20740ac43d10f090e
sairaj <naikwadisai88@gmail.com>
2024-01-26 19:17:17 +0530

    episode01

---
+++ app.js
@@ -0,0 +1,23 @@
+// import ReactDom from 'react-dom';
+
+const new_content =  React.createElement("h1",{id:"heading"},"THis Is Heading From React bhau this is feku!!");
+const ele = ReactDOM.createRoot(document.getElementById("feku"));
+
+ele.render(new_content);
+
+
+
+/* <script>
+<!--// const heading  =  document.createElement('h1');
+// heading.innerHTML = "hello bhai from javascript";
+
+// const ro  = document.getElementById("ro");
+// ro.appendChild(heading);
+
+
+// const head = React.createElement("h3",{},"Hello from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(head);-->
+
+</script> */
